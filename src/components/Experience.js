7b9f5ab8ea942a94ed84770491b05a8a0048e53c
Gotam Sai Varshith <saisaivarshith1085@gmail.com>
2025-09-30 14:01:52 +0530

    Add CSIR as the Experiance

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,6 +15,17 @@ const Experience = () => {
           <li>Successfully executed the project, demonstrating expertise in the development and deployment of AI models.</li>
         </ul>
       </div>
+      <div className="experience-item">
+        <h3>Web Developer </h3>
+        <p className="experience-details">Council of Scientific and Industrial Research | Jan 2025 - Sep 2025</p>
+        <ul className="responsibilities-list">
+          <li>Developed a fully functional, interactive educational game titled "Mathematics Masters: Lead the Way" for middle school students (Grades 6-8).</li>
+          <li>Engineered the game's front-end using HTML, Tailwind CSS, and JavaScript to build an intuitive user interface with appealing graphics and animations.</li>
+          <li>Implemented a dynamic question generation system with adaptive difficulty, featuring advanced algebraic expressions on "Green Stones" to provide strategic shortcuts.</li>
+          <li>Integrated key gamification elements, including a 90-second timed challenge, real-time scoring, and immediate visual feedback to enhance student engagement and motivation.</li>
+          <li>Collaborated within a team of four using Git for version control and followed an Agile development process over 35 weeks.</li>
+        </ul>
+      </div>
     </div>
   );
 };
